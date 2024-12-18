13649e619e85be46b7fa76eacd9f8a8145c6a5ab
jsbn <78042486+s-tn@users.noreply.github.com>
2024-12-18 09:09:09 -0500

    support page

---
+++ src/app/support/page.tsx
@@ -0,0 +1,31 @@
+import './support.css';
+
+export default function Support() {
+    return (
+        <div className="page">
+            <div className="header">
+                <div className="container">
+                    <p className="title">Support for Shipguesser</p>
+                </div>
+            </div>
+            <div className="translations-content-container">
+                <div className="container">
+                    <div className="tab-content translations-content-item en visible" id="en">
+                        <h1>Support Information</h1>
+                        <p>Last updated: December 18, 2024</p>
+                        <p>This page describes the routes that can be used to contact the developer of this app.</p>
+                        <h2>Support</h2>
+                        <p>If there are any errors with the Shipguesser app or website, you can access support information through any of three different support pathways: email, phone, mail. Bryzek development is dedicated to maintaining strong integrity with the quality of our products, and care deeply about any errors with the projects. If you have any issues with the app, please contact us immediately to resolve the issue.</p>
+                        <h2>Contact Pathways</h2>
+                        <p>Contact Name: Julien Bryzek</p>
+                        <ul>
+                            <li>By phone: 551-500-1682</li>
+                            <li>By email: jmbryzek@gmail.com</li>
+                            <li>By mail: 334 Hamilton Avenue, Glen Rock NJ 07452</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
